23820f443d5a2cbaf38c4ef034e57378044fb376
David Daeschler <david.daeschler@gmail.com>
2015-07-28 20:43:51 -0500

    Add multi-scp.sh, a tool to scp files or directories from multiple servers

---
+++ multi-scp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# copies files that are in the same location on multiple servers
+# usage: multi-scp.sh username remote_path local_path [server1 server2 server3...]
+
+if [ $# -lt 4 ]
+then
+    echo "usage: multi-scp.sh username remote_path local_path [server1 server2 server3...]"
+    exit
+fi
+
+username=$1
+remote_path=$2
+local_path=$3
+
+servers=()
+
+#collect the servers
+for i in $(seq 4 $#); do
+    scp -r $username@${!i}:$remote_path ${local_path}_${!i}
+done
