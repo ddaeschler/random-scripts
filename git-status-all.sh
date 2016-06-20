e211cc72620cf74e283325c98d40a97b4b45f30f
David Daeschler <david.daeschler@gmail.com>
2016-06-20 17:07:06 -0500

    Create git-status-all.sh

---
+++ git-status-all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# See: http://stackoverflow.com/questions/24352701/check-status-of-all-git-repositories-at-once
+
+find . -name .git -execdir git rev-parse --show-toplevel \; -execdir git status -s \;
